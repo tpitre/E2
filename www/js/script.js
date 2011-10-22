94f8fd9f81bb1ccc36ec366045a4025f0d62ef05
tpitre <tpitre@gmail.com>
2011-10-22 15:26:45 -0400

    round one of mocking up the site is done and sent

--- www/js/script.js
+++ www/js/script.js
@@ -1,13 +1,10 @@
-/* Author: Derby
-
-*/
+/* ===========================================
+   Author: Terrance Pitre
+   =========================================== */
 
 $(function(){
-	$('.hire-us').click(function () {
-		$('.contact').toggleClass('flipped');
-		$('.shadow img').toggleClass('toggle');
-	});
 	
+	// add placeholder to browsers that don't recognize them
 	if(!Modernizr.input.placeholder){
 	
 		$('[placeholder]').focus(function() {
@@ -32,43 +29,6 @@ $(function(){
 			})
 		});
 	
-	}
-	
-	$('.contact-form').submit(function(e){
-		var valid = '';
-		var name = $('#contact-name');
-		var mail = $('#contact-email');
-		var url = $('#contact-url');
-		var text = $('#contact-message');
-		
-		if (name.val().length < 1) {
-			name.addClass('alert');
-		}
-		if (!mail.val().match(/^([a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,4}$)/i)) {
-			mail.addClass('alert');
-		}
-		if (text.val().length < 1) {
-			text.addClass('alert');
-		}
-		else {
-			var datastr = 'name=' + name.val() + '&mail=' + mail.val() + '&url=' + url.val() + '&text=' + text.val();
-			$('.response').css('display', 'block').html('Sending message...').fadeIn('slow');
-			setTimeout("send('"+datastr+"')", 2000);
-		}
-		e.preventDefault();
-	});
-});
+	}	
 
-function send(datastr){
-	$.ajax({
-		type: 'POST',
-		url: 'mail.php',
-		data: datastr,
-		cache: false,
-		success: function(html){
-			$('.response').fadeIn('slow').html(html);
-			$('input, textarea').removeClass('alert');
-			setTimeout("$('.response').fadeOut('slow')", 2000);
-		}
-	});
 }
