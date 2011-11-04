651aa1b66579a3e659964db5c3731587f763135b
tpitre <tpitre@gmail.com>
2011-11-04 09:05:23 -0400

    finishing round of changs

--- www/js/script.js
+++ www/js/script.js
@@ -4,6 +4,19 @@
 
 $(function(){
 	
+	// basic modal open/close
+	$('.work-modal').click(function(e){
+		 $('.modal').fadeIn();
+		 $('.modal-bg').fadeIn();
+		 e.preventDefault();
+	});
+	
+	$('.modal .close, .modal-bg').click(function(e) {
+		$('.modal').fadeOut();
+		$('.modal-bg').fadeOut();
+		e.preventDefault();
+	})
+	
 	// add placeholder to browsers that don't recognize them
 	if(!Modernizr.input.placeholder){
 	
@@ -27,8 +40,7 @@ $(function(){
 					input.val('');
 				}
 			})
-		});
-	
+		});	
 	}	
 
-}
+});
